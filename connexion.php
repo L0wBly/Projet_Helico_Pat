d780c2b2f29c8065c40e447ce5e8d05b393da68f
Kévin <jovene.kevin@gmail.com>
2025-03-13 14:20:56 +0100

    Fix Connexion

--- connexion.php
+++ connexion.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'fonctions.php';
+session_start();
 
 if(isset($_SESSION["identifiant"])) {
     header('Location: dashboard.php');
@@ -8,8 +9,8 @@ if(isset($_SESSION["identifiant"])) {
 
 $erreurs = [];
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $identifiant = filter_input(INPUT_POST, "identifiant");
-    $motdepasse = filter_input(INPUT_POST, "motdepasse");
+    $identifiant = filter_input(INPUT_POST, "identifiant", FILTER_SANITIZE_STRING);
+    $motdepasse = filter_input(INPUT_POST, "motdepasse", FILTER_SANITIZE_STRING);
 
     if(!$identifiant) {
         $erreurs[] = "L'identifiant est obligatoire";
@@ -34,3 +35,31 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         exit();
     }
 } ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    <h1>Connexion</h1>
+    <form method="POST">
+        <label for="identifiant">Identifiant</label>
+        <input type="text" name="identifiant" id="identifiant" required>
+        <label for="motdepasse">Mot de passe</label>
+        <input type="password" name="motdepasse" id="motdepasse" required>
+        <input type="submit" value="Connexion">
+    </form>
+    <?php if(!empty($erreurs)): ?>
+        <div>
+            <ul>
+                <?php foreach ($erreurs as $erreur): ?>
+                <li><?= htmlspecialchars($erreur) ?></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    <?php endif; ?>
+</body>
+</html>

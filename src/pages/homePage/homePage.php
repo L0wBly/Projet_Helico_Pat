351dfa474c0ec478b10b96fbe99fce6814020496
Arthur <arthurboutry2005@icloud.com>
2025-03-19 13:30:46 +0100

    :art: Ajout d'un style à la home page

--- src/pages/homePage/homePage.php
+++ src/pages/homePage/homePage.php
@@ -9,10 +9,17 @@ require_once '../../controllers/fonctions.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Accueil</title>
 </head>
-<body>
-    <h1>Bienvenue sur notre site</h1>
-    <p>Veuillez vous inscrire ou vous connecter pour continuer.</p>
-    <a href="<?= BASE_URL ?>pages/inscriptionPage/inscriptionPage.php">S'inscrire</a>
-    <a href="<?= BASE_URL ?>pages/connexionPage/connexionPage.php">Se connecter</a>
+<body class="bg-sky-100">
+    <div class="flex flex-col items-center">
+        <h1 class="text-[4rem]">Bienvenue sur notre site</h1>
+        <div class="pt-[1rem] p-[10px] border-2 border-solid flex flex-col gap-[10px]">
+            <p class="text-red-500">Veuillez vous inscrire ou vous connecter pour continuer.</p>
+            <div class="flex justify-center gap-[1rem]">
+                <a href="<?= BASE_URL ?>pages/inscriptionPage/inscriptionPage.php" class="bg-gray-300" >S'inscrire</a>
+                <a href="<?= BASE_URL ?>pages/connexionPage/connexionPage.php">Se connecter</a>
+            </div>
+        </div>
+        <script src="https://cdn.tailwindcss.com"></script>
+    </div>
 </body>
 </html>

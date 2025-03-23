11f83259608fda61ed9f9b528e1388c8384638bc
L0wBly <arthurboutry2005@icloud.com>
2025-03-23 15:32:51 +0100

    :art: Ajout style connexionPage

--- src/pages/connexionPage/connexionPage.php
+++ src/pages/connexionPage/connexionPage.php
@@ -10,18 +10,25 @@ require_once '../../components/connexion/connexion.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
 </head>
-<body>
-    <h1>Connexion</h1>
-    <form method="POST">
-        <label for="pseudo">Pseudo</label>
-        <input type="mail" name="pseudo" id="pseudo" required>
-        <label for="password">Mot de passe</label>
-        <input type="password" name="password" id="password" required>
-        <input type="submit" value="Connexion">
-    </form>
+<body class="bg-sky-100">
+    <div class="w-[100%] flex flex-col items-center">
+        <h1 class="text-[4rem] text-purple-500 pt-[1.5rem]">Connexion</h1>
+        <form method="POST" class="flex flex-col justify-center items-center w-[400px] h-[35vh] rounded-[15px] m-[2rem] pt-[4rem] pb-[1rem] justify-between shadow-lg shadow-cyan-500/50 bg-sky-200">
+            <div class="flex flex-col gap-[1.5rem]">
+                <div>
+                    <label for="pseudo">Pseudo</label>
+                    <input type="mail" name="pseudo" id="pseudo" required class="rounded-[15px] pr-[5px] pl-[5px]">
+                </div>
+                <div>
+                    <label for="password">Mot de passe</label>
+                    <input type="password" name="password" id="password" required class="rounded-[15px] pr-[5px] pl-[5px]">
+                </div>
+            </div>
+            <input type="submit" value="Connexion" class="w-fit text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-[1.5rem] px-5 py-2.5 text-center me-2 mb-2">
+        </form>
 
-    <a href="<?= BASE_URL ?>pages/homePage/homePage.php">Revenir à la Home Page</a>
-
+        <a href="<?= BASE_URL ?>pages/homePage/homePage.php" class="text-[1.5rem] underline decoration-solid text-purple-500 pt-[1.5rem]">Revenir à la Home Page</a>
+    </div>
     <?php if (!empty($erreurs)): ?>
         <ul>
             <?php foreach ($erreurs as $erreur): ?>
@@ -29,5 +36,6 @@ require_once '../../components/connexion/connexion.php';
             <?php endforeach; ?>
         </ul>
     <?php endif; ?>
+    <script src="https://cdn.tailwindcss.com"></script>
 </body>
 </html>
--- src/pages/dashboardPage/dashboardPage.php
+++ src/pages/dashboardPage/dashboardPage.php
@@ -13,45 +13,47 @@ $messageErreur = isset($_GET['erreur']) ? $_GET['erreur'] : '';
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Bienvenue sur votre dashboard !</title>
 </head>
-<body>
-  <h1>Bienvenue sur votre dashboard !</h1>
+<body class="bg-sky-100">
+  <div class="w-[100%] flex flex-col items-center">
+    <h1 class="text-[4rem] text-purple-500 pt-[1.5rem]">Bienvenue sur votre dashboard !</h1>
 
-  <p>Vous êtes connecté en tant que <?= $_SESSION["identifiant"] ?></p>
+    <p>Vous êtes connecté en tant que <?= $_SESSION["identifiant"] ?></p>
 
-  <a href="<?= BASE_URL ?>pages/uploadPage/uploadPage.php">Envoyer un fichier</a>
-  <a href="<?= BASE_URL ?>components/deconnexion/deconnexion.php">Se déconnecter</a>
+    <a href="<?= BASE_URL ?>pages/uploadPage/uploadPage.php">Envoyer un fichier</a>
+    <a href="<?= BASE_URL ?>components/deconnexion/deconnexion.php">Se déconnecter</a>
 
-  <button id="changePseudoButton">Changer de pseudo</button>
+    <button id="changePseudoButton">Changer de pseudo</button>
 
-  <form id="changePseudoForm" method="POST" action="<?= BASE_URL ?>controllers/changePseudo.php" style="display: none;">
-      <label for="newPseudo">Nouveau pseudo :</label>
-      <input type="text" id="newPseudo" name="newPseudo" required>
-      <br>
-      <label for="password">Mot de passe :</label>
-      <input type="password" id="password" name="password" required>
-      <br>
-      <button type="submit">Confirmer</button>
-  </form>
+    <form id="changePseudoForm" method="POST" action="<?= BASE_URL ?>controllers/changePseudo.php" style="display: none;">
+        <label for="newPseudo">Nouveau pseudo :</label>
+        <input type="text" id="newPseudo" name="newPseudo" required>
+        <br>
+        <label for="password">Mot de passe :</label>
+        <input type="password" id="password" name="password" required>
+        <br>
+        <button type="submit">Confirmer</button>
+    </form>
 
-  <button id="changePasswordButton">Changer de mot de passe</button>
+    <button id="changePasswordButton">Changer de mot de passe</button>
 
-  <form id="changePasswordForm" method="POST" action="<?= BASE_URL ?>controllers/changePassword.php" style="display: none;">
-      <label for="currentPassword">Mot de passe actuel :</label>
-      <input type="password" id="currentPassword" name="currentPassword" required>
-      <br>
-      <label for="newPassword">Nouveau mot de passe :</label>
-      <input type="password" id="newPassword" name="newPassword" required>
-      <br>
-      <label for="confirmPassword">Confirmer le nouveau mot de passe :</label>
-      <input type="password" id="confirmPassword" name="confirmPassword" required>
-      <br>
-      <button type="submit">Confirmer</button>
-  </form>
-
+    <form id="changePasswordForm" method="POST" action="<?= BASE_URL ?>controllers/changePassword.php" style="display: none;">
+        <label for="currentPassword">Mot de passe actuel :</label>
+        <input type="password" id="currentPassword" name="currentPassword" required>
+        <br>
+        <label for="newPassword">Nouveau mot de passe :</label>
+        <input type="password" id="newPassword" name="newPassword" required>
+        <br>
+        <label for="confirmPassword">Confirmer le nouveau mot de passe :</label>
+        <input type="password" id="confirmPassword" name="confirmPassword" required>
+        <br>
+        <button type="submit">Confirmer</button>
+    </form>
+  </div>
   <?php if (!empty($messageErreur)): ?>
         <p style="color: red;"><?= $messageErreur ?></p>
   <?php endif; ?>
   
   <script src="<?= BASE_URL ?>pages/dashboardPage/dashboard.js"></script>
+  <script src="https://cdn.tailwindcss.com"></script>
 </body>
 </html>
